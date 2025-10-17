fe8e5b47a3985ccdc50b3c3eea4768fed9aa1180
VAMSHI.GUMPULA <VAMSHI.GUMPULA@PAL.TECH>
2025-10-17 09:43:40 +0000

    commit2

--- Snowflake_DBT_Integration/macros/sync_table_columns.sql
+++ Snowflake_DBT_Integration/macros/sync_table_columns.sql
@@ -1,18 +1,18 @@
 {% macro table_columns_sync(primary_col, watermark_col, source_db, source_schema, source_table, target_db, target_schema, target_table) %}
-
+ 
     {% set schema_diff_json = compare_table_schemas(source_db, source_schema, source_table, target_db, target_schema, target_table) %}
-    {% set json_str = schema_diff_json | tojson %}
-
+ 
     {% set call_stmt %}
         CALL MANAGE_TABLE_COLUMNS(
+ 
             '{{ primary_col }}',
+ 
             '{{ watermark_col }}',
-            PARSE_JSON('{{ json_str | replace("'", "''") }}')
+ 
+            PARSE_JSON('{{ schema_diff_json | replace("'", "''") | trim }}')
         );
     {% endset %}
-
-    {% do log("Executing MANAGE_TABLE_COLUMNS...", info=True) %}
+ 
     {% do run_query(call_stmt) %}
-    {% do log("Procedure executed successfully.", info=True) %}
-
+ 
 {% endmacro %}
